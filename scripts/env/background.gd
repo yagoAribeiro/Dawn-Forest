26174ce3d4844a3de3d1aaf9e9e5ec7a762a143d
YagoA <yagoamancio10@gmail.com>
2023-01-13 08:39:01 -0300

    0.0.1

--- scripts/env/background.gd
+++ scripts/env/background.gd
@@ -1,8 +1,13 @@
 extends ParallaxBackground
 class_name Background
 
-export(bool) var can_process = false
-export(Array, int) var layer_speed
+export(bool) var can_process:bool = false
+export(Array, Dictionary) var layer_obj: Array = [
+		{"layer_name": "under", "motion_offset": Vector2.ZERO, "scale":Vector2(1.0,1.0)},
+		{"layer_name": "far_away", "motion_offset": Vector2.ZERO, "scale":Vector2(1.0,1.0)},
+		{"layer_name": "middle", "motion_offset": Vector2.ZERO, "scale":Vector2(1.0,1.0)},
+		{"layer_name": "close", "motion_offset": Vector2.ZERO, "scale":Vector2(1.0,1.0)},
+	]
 
 func _ready():
 	if !can_process:
@@ -11,4 +16,7 @@ func _ready():
 func _physics_process(delta: float):
 	for child_index in get_child_count():
 		if get_child(child_index) is ParallaxLayer:
-			get_child(child_index).motion_offset.x -= delta*layer_speed[child_index]
+			for object in layer_obj:
+				if(get_child(child_index).name == object.layer_name):
+					get_child(child_index).motion_offset.x -= delta*object.motion_offset.x
+		
--- scripts/env/mask.gd
+++ scripts/env/mask.gd
@@ -2,32 +2,48 @@ extends CanvasModulate
 class_name Mask
 
 export(float, 10, 200) var day_duration
-export(bool)var menu_process
-onready var cycles = [
-		{"color": Color(1,1,1), "duration": day_duration*0.30, "name":"day"},
-		{"color": Color(0.968627, 0.67451, 0.219608), "duration": day_duration*0.15, "name":"sunfall"},
-		{"color": Color(0.12549, 0.14902, 0.188235), "duration": day_duration*0.45, "name":"night"},
-		{"color": Color(0.992157, 1, 0.760784), "duration": day_duration*0.10, "name":"sunset"}
+export(bool)var cycle_enabled:bool = true
+export(Array, Dictionary) var cycles: Array = [
+		{"color": Color(1,1,1), "duration": 0.30, "name":"day"},
+		{"color": Color(0.968627, 0.67451, 0.219608), "duration": 0.15, "name":"sunfall"},
+		{"color": Color(0.12549, 0.14902, 0.188235), "duration": 0.45, "name":"night"},
+		{"color": Color(0.992157, 1, 0.760784), "duration": 0.10, "name":"sunset"}
 	]
 enum Cycle{DAY, SUNFALL, NIGHT, SUNSET}
-var current_cycle: int = Cycle.DAY
+export var current_cycle: int = Cycle.DAY
  
 func _ready():
-	if menu_process:
+	for turn in cycles:
+		if turn.duration<1:
+			turn.duration*=day_duration
+	
+	for child in get_parent().get_children():
+		#Procura todas as background layer no parent para poder aplicar a mesma node
+		if child is ParallaxBackground and not child.find_node("Mask"):
+			child.add_child(self.duplicate())
+		
+	if cycle_enabled:
+		wait_cycle()
+
+func transition() -> void:
+	if cycle_enabled:
+		get_node("Tween").remove_all()
+		var next_cycle: int = current_cycle+1 if current_cycle+1<4 else 0
 		get_node("Tween").interpolate_property(self, "color",
-			 cycles[current_cycle].color, cycles[current_cycle+1].color,
-			 cycles[current_cycle].duration, Tween.TRANS_LINEAR,
+			 cycles[current_cycle].color, cycles[next_cycle].color,
+			 cycles[current_cycle].duration*0.5, Tween.TRANS_LINEAR,
 			 Tween.EASE_IN_OUT, 0)
 		get_node("Tween").start()	
 
+func wait_cycle() -> void:
+	if cycle_enabled:
+		get_node("wait_cycle").wait_time = cycles[current_cycle].duration*0.5
+		get_node("wait_cycle").start()
 
 func _on_Tween_tween_all_completed() -> void:
-	get_node("Tween").remove_all()
 	current_cycle = current_cycle+1 if current_cycle+1<4 else 0
-	var next_cycle: int = current_cycle+1 if current_cycle+1<4 else 0
-	get_node("Tween").interpolate_property(self, "color",
-			 cycles[current_cycle].color, cycles[next_cycle].color,
-			 cycles[current_cycle].duration, Tween.TRANS_EXPO,
-			 Tween.EASE_IN_OUT, 0)
-	get_node("Tween").start()	
-	
+	wait_cycle()
+
+
+func _on_wait_cycle_timeout():
+	transition()
